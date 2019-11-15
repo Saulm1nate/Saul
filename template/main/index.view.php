71be81532eada808f610caa0c57a2bbc5b2ae77b
Saulm1nate <saulius11mine@gmail.com>
2019-11-15 09:56:33 +0200

    first commit

---
+++ template/main/index.view.php
@@ -0,0 +1,54 @@
+<?php
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Saulius</title>
+</head>
+<body>
+<header>
+    <h1>Filmu aplikacija</h1>
+</header>
+<nav>
+    <ul>
+        <li><a href="#">Visi filmai</a></li>
+        <li><a href="#">Prieti filma</a></li>
+        <li><a href="#">Filmai pagal zanra</a></li>
+        <li><a href="#">Filmo paieska</a></li>
+        <li><a href="#">Zanru valdymas</a></li>
+    </ul>
+</nav>
+<section class="about-project">
+    <h2>Apie projekta</h2>
+    <p>Trumpai apie projekta</p>
+</section>
+<main>
+    <h2>Naujausi filmai</h2>
+    <article>
+    <p>Filmo pavadinimas</p>
+    <p>Aprasymas</p>
+    <a href="#">Daugiau...</a>
+    </article>
+    <article>
+        <p>Filmo pavadinimas</p>
+        <p>Aprasymas</p>
+        <a href="#">Daugiau...</a>
+    </article>
+    <article>
+        <p>Filmo pavadinimas</p>
+        <p>Aprasymas</p>
+        <a href="#">Daugiau...</a>
+    </article>
+
+</main>
+<footer>
+
+</footer>
+
+
+</body>
+</html>
